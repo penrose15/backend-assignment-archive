522562a5d0be28d75bd25ce1cfc16e638dcc7452
penrose15 <seongju99@naver.com>
2022-07-13 22:50:31 +0900

    레퍼런스 참고

--- src/test/java/com/codestates/homework/MemberControllerHomeworkTest.java
+++ src/test/java/com/codestates/homework/MemberControllerHomeworkTest.java
@@ -1,25 +1,37 @@
 package com.codestates.homework;
 
+import com.codestates.helper.MemberControllerTestHelper;
+import com.codestates.helper.StubData;
 import com.codestates.member.dto.MemberDto;
 import com.codestates.member.entity.Member;
 import com.codestates.member.repository.MemberRepository;
 import com.codestates.stamp.Stamp;
 import com.google.gson.Gson;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,56 +39,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MemberControllerHomeworkTest {
+public class MemberControllerHomeworkTest implements MemberControllerTestHelper {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private Gson gson;
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private ResultActions postResultActions;
     private MemberDto.Post post;
     private MvcResult postResult;
 
     @BeforeEach
     public void init() throws Exception {
-        Member member = new Member();
-        for(int i = 0; i<10; i++) {
-            member.setEmail(String.format("hhy%d@naver.com",i));
-            member.setName(String.format("하현우%d",i));
-            member.setPhone(String.format("010-1111-11%02d",i));
-            member.setStamp(new Stamp());
-            memberRepository.save(member);
-        }
-
+        this.post = (MemberDto.Post) StubData.MockMember.get(HttpMethod.POST);
+        String content = gson.toJson(post);
+        URI uri = getURI();
+        this.postResultActions = mockMvc.perform(postRequestBuilder(uri, content));
     }
-
-
-
-
-
 
     @Test
     void postMemberTest() throws Exception {
         // given
-        MemberDto.Post post = new MemberDto.Post("hgd@gmail.com",
-                "홍길동",
-                "010-1234-5678");
-        String content = gson.toJson(post);
-
-
         // when
-        ResultActions actions =
-                mockMvc.perform(
-                        post("/v11/members")
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(content)
-                );
-
         // then
-        MvcResult result = actions
+        MvcResult result = this.postResultActions
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.data.email").value(post.getEmail()))
                 .andExpect(jsonPath("$.data.name").value(post.getName()))
@@ -91,25 +78,20 @@ public class MemberControllerHomeworkTest {
         // TODO MemberController의 patchMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
 
         //given
-        MemberDto.Patch patch = new MemberDto.Patch(1L, "홍길동2", "010-1111-2222", Member.MemberStatus.MEMBER_ACTIVE);
-        patch.setMemberId(1L);
+        long memberId = getResponseMemberId();
+        MemberDto.Patch patch = (MemberDto.Patch) StubData.MockMember.get(HttpMethod.PATCH);
 
         String content = gson.toJson(patch);
+        URI uri = getURI(memberId);
         //when
         ResultActions actions = mockMvc.perform(
-                patch("/v11/members/{member-id}",1)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content)
+                patchRequestBuilder(uri,content)
         );
 
         //then
         MvcResult result = actions
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.memberId").value(patch.getMemberId()))
-                .andExpect(jsonPath("$.data.name").value(patch.getName()))
                 .andExpect(jsonPath("$.data.phone").value(patch.getPhone()))
-                .andExpect(jsonPath("$.data.memberStatus").value(patch.getMemberStatus().getStatus()))
                 .andReturn();
 
         System.out.println(result.getResponse().getContentAsString());
@@ -121,32 +103,17 @@ public class MemberControllerHomeworkTest {
     void getMemberTest() throws Exception {
         // TODO MemberController의 getMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
         //given
-        Member member = new Member("hgd@gmail.com",
-                "홍길동",
-                "010-1234-5678");
-        member.setStamp(new Stamp());
-        memberRepository.save(member);
 
 
         //when
-        ResultActions actions = mockMvc.perform(
-                get("/v11/members/{member-id}",2)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-
-        );
-
+       long memberId = getResponseMemberId();
+       URI uri = getURI(memberId);
         //then
-        MvcResult result = actions
+        mockMvc.perform(getRequestBuilder(uri))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.memberId").value(member.getMemberId()))
-                .andExpect(jsonPath("$.data.name").value(member.getName()))
-                .andExpect(jsonPath("$.data.phone").value(member.getPhone()))
-                .andExpect(jsonPath("$.data.email").value(member.getEmail()))
-                .andExpect(jsonPath("$.data.memberStatus").value(member.getMemberStatus().getStatus()))
-                .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
+                .andExpect(jsonPath("$.data.email").value(this.post.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(this.post.getName()))
+                .andExpect(jsonPath("$.data.phone").value(this.post.getPhone()));
 
     }
 
@@ -154,36 +121,28 @@ public class MemberControllerHomeworkTest {
     void getMembersTest() throws Exception {
         // TODO MemberController의 getMembers() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
 
+        String content = gson.toJson(new MemberDto.Post("hgd2@gmail.com","홍길동3","010-1111-7777"));
+        URI uri = getURI();
 
-//        for(int i = 0;i< 30;i++) {
-//            Member member1 = new Member();
-//            member1.setMemberId((long)i);
-//            member1.setPhone(String.format("010-1111-11%02d",i));
-//            member1.setEmail("hgd"+(i)+"@gmail.com");
-//            member1.setName("홍길동"+i);
-//            member1.setStamp(new Stamp());
-//            memberRepository.save(member1);
-//        }
+        mockMvc.perform(patchRequestBuilder(uri,content));
 
         //when
-        int page = 1;
-        int size = 5;
-        ResultActions actions = mockMvc.perform(
-                get("/v11/members")
-                        .param("page",String.valueOf(page))
-                        .param("size",String.valueOf(size))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
+        String page = "1"; String size = "10";
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("page", page);
+        queryParams.add("size", size);
 
-        );
+        ResultActions actions = mockMvc.perform(getRequestBuilder(uri,queryParams));
 
         //then
         MvcResult result = actions
-                .andExpect(status().isOk()) //pageinfo.totalPages
-                .andExpect(jsonPath("$.pageInfo.totalElements").value(1))
-                .andExpect(jsonPath("$.pageInfo.totalPages").value(1))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").isArray())
                 .andReturn();
+        List list = JsonPath.parse(result.getResponse().getContentAsString()).read("$.data");
+
+        assertThat(list.size(), is(1));
+
 
         System.out.println(result.getResponse().getContentAsString());
     }
@@ -191,21 +150,26 @@ public class MemberControllerHomeworkTest {
     @Test
     void deleteMemberTest() throws Exception {
         // TODO MemberController의 deleteMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
-//        Member member = new Member("hgd@gmail.com",
-//                "홍길동",
-//                "010-1234-5678");
-//        member.setStamp(new Stamp());
-//        memberRepository.save(member);
+        long memberId = getResponseMemberId();
+        URI uri = getURI(memberId);
 
-        //when
-        ResultActions actions = mockMvc.perform(
-                delete("/v11/members/{member-id}",1)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
+        mockMvc.perform(deleteRequestBuilder(uri))
+                .andExpect(status().isNoContent());
 
-        MvcResult results = actions.andExpect(status().isNoContent())
-                .andReturn();
+    }
 
+    private long getResponseMemberId() {
+        long memberId;
+        try {
+            String responseMember = this.postResultActions
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+            memberId = JsonPath.parse(responseMember).read("$.data.memberId",Long.class);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return memberId;
     }
 }

f1bb914c365d9e24b0bb170714501a6f6e0f9aca
hsj <bukang@breezelab.io>
2024-03-14 11:29:17 +0900

    practice transaction

--- src/main/java/com/codestates/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.order.repository;
-
-import com.codestates.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
